f52cf8ff55cc3848a77ca139cec02f90d6617049
valentin <valentin@42angouleme.fr>
2024-03-24 15:13:09 +0100

    wip: Doc comments Time

--- src/time.rs
+++ src/time.rs
@@ -3,14 +3,16 @@ use std::ops::{Deref, DerefMut};
 use chrono::{Local, NaiveDateTime, NaiveTime, TimeDelta, Timelike};
 use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 
-const BASE_TIME_FORMAT: &str = "%H:%M:%S";
+pub const BASE_TIME_FORMAT: &str = "%H:%M:%S";
 
+/// [Serialize] the [NaiveTime] variable from [Time]
 pub fn time_to_str<S: Serializer>(time: &NaiveTime, serializer: S) -> Result<S::Ok, S::Error> {
     time.format(BASE_TIME_FORMAT)
         .to_string()
         .serialize(serializer)
 }
 
+/// [Deserialize] the [NaiveTime] variable from [Time]
 pub fn time_from_str<'de, D>(deserializer: D) -> Result<NaiveTime, D::Error>
 where
     D: Deserializer<'de>,
@@ -19,20 +21,19 @@ where
     NaiveTime::parse_from_str(&time, BASE_TIME_FORMAT).map_err(de::Error::custom)
 }
 
+/// Unit to update [Time]
 #[derive(Debug, Clone)]
-enum TimeUnit {
+pub enum TimeUnit {
     Hour,
     Minute,
     Second,
 }
 
-/// Time structure to handle time management
+// Structure to handle time management
 ///
-/// const BASE_TIME_FORMAT: &str = "%H:%M:%S";
-///
-/// BASE_TIME_FORMAT is the default format for time
+/// Use [BASE_TIME_FORMAT] as default format for time
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct Time {
+pub struct Time {
     #[serde(serialize_with = "time_to_str", deserialize_with = "time_from_str")]
     pub time: NaiveTime,
     pub format: String,
@@ -65,6 +66,21 @@ impl Default for Time {
 }
 
 impl Time {
+    /// Create a new variable Time from the parameters `time` and `format`
+    ///
+    ///  See the [chrono::format::strftime] for the supported escape sequences of `format`.
+    ///
+    /// # Example
+    ///
+    /// ```rust,no_run
+    /// let time = Time::new("09_27_00", "%H_%M_%S")?;
+    ///
+    /// assert_eq!(Time::build("11:35:19"), Time::new("11:35:19", BASE_TIME_FORMAT))
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// Return an Err(_) if `time` is not formated with `format`
     pub fn new(time: impl ToString, format: impl ToString) -> Result<Self, String> {
         let time = match NaiveTime::parse_from_str(&time.to_string(), &format.to_string()) {
             Ok(time) => time,
@@ -76,19 +92,37 @@ impl Time {
         })
     }
 
+    /// Create a new variable Time from the parameter `time`,
+    /// if the time is formated with `BASE_TIME_FORMAT`
+    ///
+    /// # Example
+    ///
+    /// ```rust,no_run
+    /// let time = Time::build("09:05:12")?;
+    ///
+    /// assert_eq!(Time::build("00:00:00"), Time::midnight())
+    /// assert_eq!(Time::build("00:00:00"), Time::default())
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// Return an Err(_) if the given `time` is not formated with `BASE_TIME_FORMAT`
     pub fn build(time: impl ToString) -> Result<Self, String> {
         Self::new(time, BASE_TIME_FORMAT)
     }
 
+    /// Getter for the `time` [NaiveTime] variable
     pub fn time(&self) -> NaiveTime {
         self.time
     }
 
+    /// Setter for the `format` [String] variable
     pub fn format(mut self, format: impl ToString) -> Self {
         self.format = format.to_string();
         self
     }
 
+    /// Function to increase / decrease the time [Time] with [TimeUnit]
     pub fn update(&mut self, unit: TimeUnit, value: i32) -> Result<(), String> {
         let delta_time = match unit {
             TimeUnit::Hour => TimeDelta::new(value as i64 * 60 * 60, 0),
@@ -107,22 +141,34 @@ impl Time {
         }
     }
 
+    /// Go to the next [TimeUnit] from [Time]
     pub fn next(&mut self, unit: TimeUnit) -> Result<(), String> {
         self.update(unit, 1)
     }
 
-    pub fn matches(&self, unit: TimeUnit, value: u32) -> Result<bool, String> {
+    /// Compare the [TimeUnit] from [Time] and value ([u32])
+    pub fn matches(&self, unit: TimeUnit, value: u32) -> bool {
         match unit {
-            TimeUnit::Hour => Ok(self.time.hour() == value),
-            TimeUnit::Minute => Ok(self.time.minute() == value),
-            TimeUnit::Second => Ok(self.time.second() == value),
+            TimeUnit::Hour => self.time.hour() == value,
+            TimeUnit::Minute => self.time.minute() == value,
+            TimeUnit::Second => self.time.second() == value,
         }
     }
 
+    /// Return the current [Time]
     pub fn now() -> Result<Self, String> {
         Self::build(Local::now().format(BASE_TIME_FORMAT))
     }
 
+    /// Return midnight [Time]
+    ///
+    /// # Example
+    ///
+    /// ```rust,no_run
+    ///
+    /// assert_eq!(Time::build("00:00:00"), Time::midnight())
+    /// assert_eq!(Time::build("00:00:00"), Time::default())
+    /// ```
     pub fn midnight() -> Self {
         let time = NaiveTime::from_hms_opt(0, 0, 0).expect("Error Time midnight");
         Self {
@@ -131,14 +177,17 @@ impl Time {
         }
     }
 
+    /// Return a [bool] to know if the [Time] is in the future
     pub fn is_in_future(&self) -> Result<bool, String> {
         Ok(self.time > Self::now()?.time)
     }
 
+    /// Elapsed [TimeDelta] between two [Time]
     pub fn elapsed(&self, lhs: &Self) -> TimeDelta {
         self.time.signed_duration_since(lhs.time)
     }
 
+    /// Number of [TimeUnit] between two [Time]
     pub fn unit_in_between(&self, unit: TimeUnit, lhs: &Self) -> i64 {
         match unit {
             TimeUnit::Hour => self.time.hour() as i64 - lhs.time.hour() as i64,
@@ -309,6 +358,20 @@ pub mod test {
         };
         assert_eq!(time.to_string(), "T12_21_46".to_string());
         assert_eq!(time.format, "T%H_%M_%S".to_string());
+        Ok(())
+    }
+
+    #[test]
+    fn test_time_default_equal_midnight() -> Result<(), String> {
+        let time_built = Time::build("00:00:00")?;
+        let midnight = Time::midnight();
+        let default = Time::default();
+        assert_eq!(time_built.to_string(), midnight.to_string());
+        assert_eq!(time_built.format, midnight.format);
+        assert_eq!(time_built.to_string(), default.to_string());
+        assert_eq!(time_built.format, default.format);
+        assert_eq!(midnight.to_string(), default.to_string());
+        assert_eq!(midnight.format, default.format);
         Ok(())
     }
 }
